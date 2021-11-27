b2ae33b87972b62e8e7c7a4dbbfb110e60eef4d0
Vinicius Sodré <vinicius.sodre@somagrupo.com.br>
2021-11-27 03:48:15 -0300

    base dashboard sem estilização

---
+++ carbonfree/src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from './assets/logo';
+
+function Logo() {
+    return (
+        <div>
+            <img src={logo} alt="Carbonfree Logo" />
+        </div>
+    )
+}
+
+export default Logo
---
+++ carbonfree/src/pages/App/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const App = styled.div`
+    background=black;
+    display:flex;
+    
+`
