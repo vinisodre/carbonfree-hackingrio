f3c936d9ca18053b6f7d5a92103209795ad37e7b
Eduarda Terra <eduardapterra98@gmail.com>
2021-11-27 12:31:34 -0300

    Home Content created and styled

--- carbonfree/src/pages/Home/Home.jsx
+++ carbonfree/src/pages/Home/Home.jsx
@@ -1,11 +1,24 @@
 import * as S from "./styles"
 import Header from "../../components/Header/Header"
+import planet from "../../assets/images/planet.svg"
 
 const Home = () => {
 
   return<>
+  <S.Container>
   <Header />
-  <S.Container><h1>teste</h1></S.Container>
+    <S.ContentContainer>
+    <img src={planet}/>
+    <S.TextContainer>
+      <S.Title>
+      Lorem ipsum dolor <strong>sit ame</strong>
+      </S.Title>
+      <S.Paragraph>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+      </S.Paragraph>
+    </S.TextContainer>
+    </S.ContentContainer>
+  </S.Container>
   </>
 }
 
--- carbonfree/src/pages/Home/styles.js
+++ carbonfree/src/pages/Home/styles.js
@@ -1,7 +1,45 @@
 import styled from "styled-components"
 
 export const Container = styled.section`
-height: 120vh;
+display:flex;
+flex-direction: column;
+min-height: 100vh;
 width: 100%;
-background:blue;
+background:#002344;
+`
+
+export const ContentContainer = styled.div`
+display:flex;
+align-items:center;
+justify-content:space-evenly;
+width: 100%;
+height: 89.5vh;
+> img{
+width: 440px;
+}
+`
+
+export const TextContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+justify-content:space-evenly;
+height: 400px;
+width: 400px;
+`
+
+export const Title = styled.h2`
+font-size: 48px;
+color: white;
+>strong {
+  color: #109655
+}
+`
+
+export const Paragraph = styled.p`
+font-size: 18px;
+color: white;
+>strong {
+  color: #109655
+}
 `
