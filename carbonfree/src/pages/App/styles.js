ca8dc0f52bff9d469cffa7c7cbf97a60970fd625
Eduarda Terra <eduardapterra98@gmail.com>
2021-11-27 04:30:45 -0300

    Bug Fix UserDashboard

--- carbonfree/src/pages/App/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const App = styled.div`
-    background=black;
-    display:flex;
-    
-`
--- carbonfree/src/pages/UserDashboard/UserDashboard.jsx
+++ carbonfree/src/pages/UserDashboard/UserDashboard.jsx
@@ -1,15 +1,13 @@
-import Header from "../";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Main from "../../components/Main/Main";
 import * as S from './styles';
 
 function UserDashboard() {
   return (
-    <S.App>
-      <Header />
+    <S.Container>
       <Sidebar/>
       <Main />
-    </S.App>
+    </S.Container>
   );
 }
 
